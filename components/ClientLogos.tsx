0d36eaf52cc8a550259f09c5e049120fe1080bc1
lamovie-agencia <lamovie.agencia.creativa@gmail.com>
2026-05-25 09:56:37 -0500

    logos

--- components/ClientLogos.tsx
+++ components/ClientLogos.tsx
@@ -7,7 +7,8 @@ const FALLBACK_LOGOS = [
   { name: "Bahia del Sol", logo_url: "https://static.wixstatic.com/media/fd0487_5c8e753e52084b6d8d35d69cc656e65c~mv2.png/v1/crop/x_0,y_894,w_1641,h_923/fill/w_128,h_70,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/logo-letras-azules.png" },
   { name: "BHK", logo_url: "https://cdn.shopify.com/s/files/1/0700/3052/4636/files/LOGO_BHK_3.png?v=1766181369" },
   { name: "La Vitrina Textil", logo_url: "https://www.lavitrinatextil.com/cdn/shop/files/LOGO_LA_VITRINA_TEXTIL_SINFONDO-02.png?v=1770673573&width=1920" },
-  { name: "Gets Mobile", logo_url: "https://getsmobile.shop/wp-content/uploads/2024/06/cropped-logogRecurso-5.png" }
+  { name: "Gets Mobile", logo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNBl9hqGvovWp2aZV69qQf7KSoCI49ws9m4A&s" },
+    { name: "Grupo Educate", logo_url: "https://www.grupoeducatecolombia.com/wp-content/uploads/2025/10/menu-logo-K.png" }
 ];
 
 export default function ClientLogos() {
